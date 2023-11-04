11944ffbe1b0e8e7fb183072c566a8faa5da3c8e
David Röger <david@codeatelier.com>
2023-11-04 02:45:34 +0100

    add initial setup

---
+++ packages/svelte-store/vite.config.ts
@@ -0,0 +1,16 @@
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  plugins: [svelte()],
+  test: {
+    name: 'svelte-store',
+    watch: false,
+    globals: true,
+    coverage: { provider: 'istanbul' },
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,ts}'],
+    exclude: ['**/node_modules/**', '**/build/**','**/coverage/**','**/.svelte-kit/**',],
+    setupFiles: ['test-setup.ts'],
+  },
+})
